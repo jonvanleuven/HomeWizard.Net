b4765a07fcf8324118a42c67c63449294de7f6ee
Sven <sven.bluethoot@gmail.com>
2018-05-11 19:04:58 +0200

    Added thermometer properties

--- HomeWizard.Net/Models/ThermoMeter.cs
+++ HomeWizard.Net/Models/ThermoMeter.cs
@@ -1,7 +1,38 @@
-﻿namespace HomeWizard.Net
+﻿using System;
+using Newtonsoft.Json;
+using HomeWizard.Net.Converters;
+
+namespace HomeWizard.Net
 {
     public class ThermoMeter : Device
     {
-        
+        public int Channel { get; set; }
+        public int Model { get; set; }
+        public long Code { get; set; }
+
+        [JsonProperty("te")]
+        public double Temperature { get; set; }
+        [JsonProperty("te+")]
+        public double HighestTemperature { get; set; }
+        [JsonProperty("te+t")]
+        public DateTime HighestTemperatureTime { get; set; }
+        [JsonProperty("te-")]
+        public double LowestTemperature { get; set; }
+        [JsonProperty("te-t")]
+        public DateTime LowsetTemperatureTime { get; set; }
+
+        [JsonProperty("hu")]
+        public int Humidity { get; set; }
+        [JsonProperty("hu+")]
+        public int HighestHumidity { get; set; }
+        [JsonProperty("hu+t")]
+        public DateTime HighestHumidityTime { get; set; }
+        [JsonProperty("hu-")]
+        public int LowestHumidity { get; set; }
+        [JsonProperty("hu-t")]
+        public DateTime LowestHumidityTime { get; set; }
+
+        [JsonConverter(typeof(BooleanConverter))]
+        public bool Outsite { get; set; }
     }
 }
